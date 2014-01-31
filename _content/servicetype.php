945ad45b4c8c2d1a1c94364cfb0b6977815f052b
Grant Bartlett <grant@smswmedia.com>
2014-01-31 16:05:36 +0000

    Initial commit

---
+++ _content/servicetype.php
@@ -0,0 +1,75 @@
+<div id="panel2" class="content active">
+    <dl class="tabs" data-tab>
+        <dd><a href="#servicetype2-1">Editing, Publishing and Saving </a></dd>
+        <dd><a href="#servicetype2-4">Pages </a></dd>
+        <dd><a href="#servicetype2-2">Find a Service </a></dd>
+        <dd><a href="#servicetype2-3">Slide Content </a></dd>
+    </dl>
+
+    <div class="tabs-content">
+
+        <div class="content" id="servicetype2-1">
+            <p>
+                Login to the admin panel using your username and password.
+            <ol>
+                <li>Content > Publish/Edit > Service Type.</li>
+                <li>Enter Service Type Data.</li>
+                <li>Click Publish to save your changes.</li>
+            </ol>
+            </p>
+        </div>
+
+        <div class="content" id="servicetype2-2">
+            <p>
+                <div class="panel callout radius">
+                    <h5>Please note</h5>
+                    <p>Enabling 'Find a Service' will only display when there is no Slider Content present.</p>
+                </div>
+                <ol>
+                    <li>Set Find a Service, to Yes or No</li>
+                    <li>Click Publish to save your changes.</li>
+                </ol>
+                <img src="_content/images/servicetype_findaservice.png"/>
+            </p>
+            <div class="panel callout radius">
+                <h5>Result</h5>
+            </div>
+            <img src="_content/images/findaserviceresult.png"/>
+        </div>
+
+        <div class="content" id="servicetype2-3">
+            <p>
+                <div class="panel callout radius">
+                    <p>Select a Success Story to appear in the slider. Drag the green selected items in order from top to bottom, to display first and last.</p>
+                </div>
+                <ol>
+                    <li>Select a Success Story</li>
+                    <li>For a Success Story to appear first, drag an item to the top.</li>
+                    <li>Click Publish to save your changes.</li>
+                </ol>
+                <img src="_content/images/servicetype_slider.png"/>
+            </p>
+            <div class="panel callout radius">
+                <h5>Result</h5>
+            </div>
+            <img src="_content/images/servicetype_slider_result.png"/>
+        </div>
+
+
+        <div class="content" id="servicetype2-4">
+            <p>
+                <ol>
+                    <li>Select or Search for a page to display on the Service Type</li>
+                    <li>For a Service Type to appear first, drag an item to the top.</li>
+                    <li>Click Publish to save your changes.</li>
+                </ol>
+            <img src="_content/images/servicetype_pages.png"/>
+            </p>
+            <div class="panel callout radius">
+                <h5>Result</h5>
+            </div>
+            <img src="_content/images/servicetype_pages_result.png"/>
+        </div>
+
+    </div>
+</div>
