c4f91688bc51f65c9d0a75b6596b79862f5c572f
Grant Bartlett <grant@smswmedia.com>
2014-02-26 15:52:13 +0000

    Book a session/ Product tutorials. Pasting content into Editor.

--- _content/editor.php
+++ _content/editor.php
@@ -12,14 +12,14 @@
             <h3>Pasting from Word Documents, Websites, and other misc sources.
             </h3>
             <p>Here are a few tips to help you copy content into the Editor CMS.</p>
-            <p>Often when copying from documents or websites, your clipboard will also carry over any styling you have on the page. The editor will take what you have pasted and try to replicate it as per your page.</p>
+            <p>Often when copying from documents or websites, your clipboard will also carry over any styling you have on the page. The editor will take what you have pasted and try to replicate it as per your Word doc/website. This will of course cause your pages to display incorrectly.</p>
             <p>Select a Tip, that most works for you.</p>
         </div>
 
         <!-- Tip 1 -->
         <div class="content" id="products-panel2-6">
             <h3>Tip 1 - Pasting directly into the CMS.</h3>
-            <p>Unless it is <a href="http://en.wikipedia.org/wiki/Text_editor" title="plain text wiki" target="_blank">plain text</a>, pasting directly into the CMS from a Word document or website will need some tricks to get it to perform correctly.</p>
+            <p>Unless it is <a href="http://en.wikipedia.org/wiki/Text_editor" title="plain text wiki" target="_blank">plain text</a>, pasting directly into the CMS from a Word document or website will need some help to get it to display correctly.</p>
             <p>Pasting directly from websites/documents will often bring unwanted formatting and tags the editor is unable to determine.</p>
             <ol>
                 <li>After copying your content/text from a website or document, select (usually a Description field) and paste (Ctrl+v/cmd+v) it into the description field.</li>
@@ -33,7 +33,7 @@
         <!-- Tip 2 -->
         <div class="content" id="products-panel2-2">
             <h3>Tip 2 - Pasting into Notepad, and then into the CMS.</h3>
-            <p>Unless it is <a href="http://en.wikipedia.org/wiki/Text_editor" title="plain text wiki" target="_blank">plain text</a>, pasting directly into the CMS from a Word document or website will need some tricks to get it to perform correctly.</p>
+            <p>Unless it is <a href="http://en.wikipedia.org/wiki/Text_editor" title="plain text wiki" target="_blank">plain text</a>, pasting directly into the CMS from a Word document or website will need some help to get it to display correctly.</p>
             <p>Pasting directly from websites/documents will often bring unwanted formatting and tags the editor is unable to determine.</p>
             <ul>
                 <li>After copying your content/text from a website or document, open Notepad on your computer. (Note: Not Microsoft Word or any other application that formats your text)</li>
