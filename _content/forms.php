bb0d344710390055d683927c5f50153174c8f4d4
Grant Bartlett <grant@smswmedia.com>
2014-04-29 15:25:42 +0100

    Forms doc.

---
+++ _content/forms.php
@@ -0,0 +1,81 @@
+<div id="panel14" class="content">
+    <dl class="tabs" data-tab>
+        <dd class="active"><a href="#forms-intro">Introduction</a></dd>
+        <dd><a href="#forms-1">Creating a new form</a></dd>
+        <dd><a href="#forms-2">Adding Text</a></dd>
+        <dd><a href="#forms-4">Adding a form to a page</a></dd>
+    </dl>
+
+    <div class="tabs-content">
+        <div class="content active" id="forms-intro">
+            <h3>Login to the <a href="http://regard.co.uk/admin.php" title="Admin panel" rel="nofollow" target="_blank">admin panel</a> using your username and password.
+            </h3>
+            <h5>Icons explained</h5>
+            <img src="/_content/images/forms_buttons.png">
+            <br>
+            <ol>
+                <li>Composer, this is the icon to edit existing forms.</li>
+                <li>Edit, this is the icon to edit the form details, such as notification details.</li>
+                <li>Duplciate, this is the icon to duplicate an existing form. </li>
+                <li>To delete a form and all of its submissions.</li>
+            </ol>
+        </div>
+
+        <div class="content" id="forms-1">
+            <h3>To create a new form:</h3>
+            <ol>
+                <li>Navigate to the admin panel, and click Forms in the menu.</li>
+                <li>Click the pink ‘New Form’ button</li>
+                <li>Make sure Form Type is set to Composer.</li>
+            </ol>
+            <p>Or to copy an existing form, find the Duplicate column label and click the ‘Duplicate’ icon next to the form you wish to copy.</p>
+
+            <h3>Getting setup</h3>
+            <ol>
+                <li>Enter the name of the form.  Form Name will autocomplete so is not necessary to enter anything.</li>
+                <li>To notify a member of staff about a submission to your form, check ‘Notify Admin’. Just below, add separated by commas, the email addresses you wish to notify of any new form submissions</li>
+                <li>Click the pink ‘Continue’ button to carry onto the next step.</li>
+
+                <li>The next screen you will be presented with will be where you can drag and drop fields to create your form.</li>
+                <li>Make sure Form Type is set to Composer.</li>
+                <li>Under, Composer Template dropdown, select ‘General’.
+                    <br class="clear">
+                    <img src="/_content/images/forms_composertemplate.png">
+                </li>
+            </ol>
+
+        </div>
+
+        <div class="content" id="forms-2">
+            <h3>Adding text</h3>
+            <ol>
+                <li>To insert text, drag ‘Paragraph’ and drop to where you would like it to display.
+                    <br>
+                    <img src="/_content/images/forms_paragraph.png">
+                </li>
+                <li>It will now display 'Double Click to Edit'. Double click once dropped into place to be able add content, clicking 'Finished' once happy.</li>
+            </ol>
+
+            <h3>Adding Fields</h3>
+            <p>Create a new field or simply search for existing ones made from other forms. </p>
+            <ol>
+                <li>Create a new field by clicking the pink 'New Field' button, and completing the list of options given to you. The field types you should use are Text (Restricts to one line), and Text Area (Used for multiple lines). Once happy, click 'Create Field'.</li>
+                <li>In the sidebar, you can search or drag a field to your form (E.g search for email). Once found, you can simply drag this field on to your form.</li>
+                <li>Once happy, click 'Quick save' to save your progress and to continue working on your form. Otherwise click 'Save and Finish' to save and finish editing.</li>
+            </ol>
+        </div>
+
+        <div class="content" id="forms-4">
+            <h3>Add a form to a page</h3>
+            <p>Creating a new page can be found in the 'Pages and Navigation' section of the documentation. </p>
+            <p>
+                <img src="/_content/images/forms_form.png">
+            </p>
+            <ol>
+                <li>To add a form to your page, click the 'Form' tab when publishing/editing a page.</li>
+                <li>Within the dropdpown you will be given a list of forms to display in your page.</li>
+                <li>Select a form to display in your page, and click Submit to finish.</li>
+            </ol>
+        </div>
+    </div>
+</div>
