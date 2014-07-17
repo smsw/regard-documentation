f165d9e45eee8d4eaf6c1681e66a7e25ea26374a
Grant Bartlett <grant@smswmedia.com>
2014-07-17 11:09:01 +0100

    Download LInks

---
+++ _content/links.php
@@ -0,0 +1,31 @@
+<div id="panel15" class="content">
+    <dl class="tabs" data-tab>
+        <dd class="active"><a href="#link-intro">Introduction</a></dd>
+        <dd><a href="#editing-link-1">Adding download links to your content.</a>
+    </dl>
+
+    <div class="tabs-content">
+        <div class="content active" id="link-intro">
+            <h3>Login to the <a href="http://regard.co.uk/admin.php" title="Admin panel" rel="nofollow" target="_blank">admin panel</a> using your username and password.
+            </h3>
+        </div>
+        <div class="content" id="editing-link-1">
+            <h3>Download Links</h3>
+            <div class="panel callout radius">
+                You can put a download link within the body of the copy.
+            </div>
+            <ol>
+                <li>Type download pdf or something similar into the page body.</li>
+                <li>Highlight that text so it can be a link. <br>
+                    <img src="/_content/images/downloadlinks_highlight.png" alt="highlight text"/>
+                </li>
+                <li>Click the link icon
+                    <ul>
+                        <li>Add the direct download link to the pdf </li>
+                    </ul>
+                </li>
+                <li>Click update link, and then submit. It will then appear on your page.</li>
+            </ol>
+        </div>
+    </div>
+</div>
